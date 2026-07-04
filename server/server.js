e4785fba2242f97df6a1231fe2ac0c2b9189f9cf
tejaswiniaher <tejaswiniaher905@gmail.com>
2026-07-04 13:07:07 +0530

    fix: connect server.js to MongoDB via connectDB()

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app.js';
-
+import connectDB from './config/database.js';
 import logger from './utils/logger.js';
 
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
